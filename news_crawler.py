3ad8578d7fa33e4f654f52346d608cad50be9e84
SejutiD <duttasejuti24@gmail.com>
2024-05-09 16:43:58 +0200

    took the JSON extractor in a different class

--- news_crawler.py
+++ news_crawler.py
@@ -9,40 +9,41 @@ class NewsSpider(scrapy.Spider):
     start_urls = ["https://news.ycombinator.com/"]
 
     def parse(self, response):
-        ENTRY_SELECTOR = '//tr[@class="athing"]'
-        ORDER_SELECTOR = './/span[@class="rank"]/text()'
-        TITLE_SELECTOR = './/span[@class="titleline"]/a/text()'
-        COMMENTS_SELECTOR = './/a[contains(text(), "comments")]/text()'
-        POINTS_SELECTOR = './/span[@class="score"]/text()'
-
-        for entry in response.xpath(ENTRY_SELECTOR):
-            order_text = entry.xpath(ORDER_SELECTOR).get()
-            order = int(order_text.strip("."))
-            title = entry.xpath(TITLE_SELECTOR).get()
-
-            next_sibling = entry.xpath("./following-sibling::tr")[0]
-            comments_text = next_sibling.xpath(COMMENTS_SELECTOR).get()
-            comments = (
-                int(re.search(r"\d+", comments_text).group()) if comments_text else 0
-            )
-
-            points_text = next_sibling.xpath(POINTS_SELECTOR).get()
-            points = int(re.search(r"\d+", points_text).group()) if points_text else 0
-
+        for entry in self.get_entry_elements(response):
+            order = self.extract_order(entry)
+            title = self.extract_title(entry)
+            comments, points = self.extract_comments_and_points(entry)
             yield NewsEntry(
                 order=order, title=title, total_comments=comments, points=points
             )
 
+    def get_entry_elements(self, response):
+        return response.xpath('//tr[@class="athing"]')
 
-process = CrawlerProcess(
-    settings={
-        "FEEDS": {
-            "output.json": {
-                "format": "json",
-            },
-        },
-    }
-)
+    def extract_order(self, entry):
+        order_text = entry.xpath('.//span[@class="rank"]/text()').get()
+        return int(order_text.strip("."))
 
+    def extract_title(self, entry):
+        return entry.xpath('.//span[@class="titleline"]/a/text()').get()
+
+    def extract_comments_and_points(self, entry):
+        next_sibling = entry.xpath("./following-sibling::tr")[0]
+        comments_text = next_sibling.xpath(
+            './/a[contains(text(), "comments")]/text()'
+        ).get()
+        comments = int(re.search(r"\d+", comments_text).group()) if comments_text else 0
+
+        points_text = next_sibling.xpath('.//span[@class="score"]/text()').get()
+        points = int(re.search(r"\d+", points_text).group()) if points_text else 0
+
+        return comments, points
+
+
+custom_settings = {
+    "ITEM_PIPELINES": {"pipelines.JsonExportPipeline": 1},
+}
+
+process = CrawlerProcess(custom_settings)
 process.crawl(NewsSpider)
 process.start()
